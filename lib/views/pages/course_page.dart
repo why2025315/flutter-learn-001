24c1263e9c99909c93f2e88c47ac3311b222da2a
王红彦 <957737702@qq.com>
2026-01-12 18:03:08 +0800

    feat: 请求

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,19 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+import '../../data/classes/activity_class.dart';
 import '../../widgets/hero_widget.dart';
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Future getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return Activity.fromJson(convert.jsonDecode(response.body));
+    } else {
+      log('Request failed with status: ${response.statusCode}.');
+      throw Exception('请求失败: ${response.statusCode}: ${response.body}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('course-page')),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(children: [HeroWidget(title: 'Course Page')]),
-        ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data; // 获取数据
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    HeroWidget(title: activity.activity),
+                    Text(activity.activity),
+                  ],
+                ),
+              ),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          return Center(child: Text('Result: ${snapshot.data}'));
+        },
       ),
     );
   }
